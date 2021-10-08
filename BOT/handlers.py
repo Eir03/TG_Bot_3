de46bbfd911d4b1c896d59981ad70899b6965533
Eir03 <71316160+Eir03@users.noreply.github.com>
2021-10-08 14:00:47 +0300

    fgfdgfdgfdgfdgfdgfd

--- BOT/handlers.py
+++ BOT/handlers.py
@@ -9,7 +9,6 @@ from aiogram.types import Message
 keyboard_markup = types.ReplyKeyboardMarkup(row_width=3)
 array_keyboard = ['Кнопочка', 'Съесть почку']
 
-
 # Send message to admin
 async def send_to_admin(dp):
 	await bot.send_message(chat_id=admin_id, text="Bot start")
@@ -18,16 +17,17 @@ async def send_to_admin(dp):
 async def send_welcome(message: types.Message):
         keyboard_markup.add(*(types.KeyboardButton(text) for text in array_keyboard))
         await message.answer(text='Здрасть',reply_markup=keyboard_markup)
+        array_keyboard.clear()
         statistic(message.chat.id,message.text)
         stat(message.chat.id,message.text)
 
-def stat(user_id,command):
-    connection = sqlite3.connect('DataB.db')
-    cursor = connection.cursor()
-    data = datetime.datetime.today().strftime("%Y-%m-d %H:%M")
-    cursor.execute("INSERT INTO stat(user_id, user_command, date) VALUES ('%s''%s''%s')" % (user_id, command, data))
-    connection.comit()
-    cursor.close()
+def stat(user_id, command):
+        conn = sqlite3.connect('DataBa.db')
+        cursor = conn.cursor()
+        data = datetime.datetime.today().strftime("%d-%m-%Y %H:%M")
+        cursor.execute("INSERT INTO stata(user_id, user_command, date) VALUES ('%s', '%s', '%s')" % (user_id, command, data))
+        conn.commit()
+        cursor.close()
 
 def statistic(user_id, command):
     data = datetime.datetime.today().strftime("%Y-%m-d %H:%M")
